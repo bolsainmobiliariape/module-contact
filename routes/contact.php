b46843256ddd88ccbe939eb791cd168e362517d1
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2022-02-09 10:35:54 -0600

    auth middleware

--- routes/contact.php
+++ routes/contact.php
@@ -5,7 +5,7 @@ use Bolsainmobiliariape\ModuleContact\Http\Livewire\Dashboard\Contact\Index;
 use Bolsainmobiliariape\ModuleContact\Http\Livewire\Dashboard\Contact\Show;
 use Illuminate\Support\Facades\Route;
 
-Route::as('dashboard.contact.')->middleware(['web'])->prefix('/dashboard/contact')->group(function (){
+Route::as('dashboard.contact.')->middleware(['web', 'auth'])->prefix('/dashboard/contact')->group(function (){
     Route::get('/', [IndexController::class, 'index'])->name('index');
     Route::get('{contact}', Show::class)->name('show');
 });
