e9dae1d1b61fb128c8e359fcebe37e4522ab5cef
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2022-02-22 23:43:24 -0600

    Command for publish stubs

--- src/Commands/ModuleContactCommand.php
+++ src/Commands/ModuleContactCommand.php
@@ -6,13 +6,17 @@ use Illuminate\Console\Command;
 
 class ModuleContactCommand extends Command
 {
-    public $signature = 'module-contact';
+    public $signature = 'module:contact';
 
-    public $description = 'My command';
+    public $description = 'Publish config and migration';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        
+        $this->callSilent('vendor:publish', ['--tag'=>'module-contact-config']);
+        $this->callSilent('vendor:publish', ['--tag'=>'module-contact-migrations']);
+
+        $this->comment('Published module-contact config file and migration');
 
         return self::SUCCESS;
     }
--- src/ModuleContactServiceProvider.php
+++ src/ModuleContactServiceProvider.php
@@ -4,6 +4,7 @@ namespace Bolsainmobiliariape\ModuleContact;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Bolsainmobiliariape\ModuleContact\Commands\ModuleContactCommand;
 use Bolsainmobiliariape\ModuleContact\Http\Livewire\Dashboard\Contact\Index;
 use Bolsainmobiliariape\ModuleContact\Http\Livewire\Dashboard\Contact\Show;
 use Livewire\Livewire;
@@ -24,6 +25,7 @@ class ModuleContactServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_module_contact_table')
-            ->hasRoute('contact');
+            ->hasRoute('contact')
+            ->hasCommand(ModuleContactCommand::class);
     }
 }
